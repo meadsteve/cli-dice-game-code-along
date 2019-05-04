aee463a9fc1fc179a182dadacc2faefafd74e397
steve <meadsteve@gmail.com>
2019-05-04 14:10:43 +0200

    add inquirer and ask for the number of players

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const inquirer = require("inquirer");
+
 const run = async () => {
-    console.log("Hello. Let's play a game of liar's dice.")
+    console.log("Hello. Let's play a game of liar's dice.");
+    let answers = await inquirer.prompt([
+        {
+            name: "playerCount",
+            type: "number",
+            message: "How many players are there?",
+        },
+    ]);
+    console.log(answers);
 };
 
 run();
