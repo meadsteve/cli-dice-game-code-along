05e7496185167bd49ce6be248f7b6af7ef691621
steve <meadsteve@gmail.com>
2019-05-10 08:37:00 +0200

    give each player a turn

--- index.js
+++ index.js
@@ -27,11 +27,27 @@ const setupGame = async () => {
     }
 };
 
+const getCurrentPlayer = (game) => {
+    return game.players[game.active_player_index];
+};
+
+const moveToNextPlayer = (game) => {
+    game.active_player_index = (game.active_player_index + 1) % game.players.length;
+    return game;
+};
+
+const takeTurn = async (game) => {
+    const player = getCurrentPlayer(game);
+    console.log(`${player.name} it's your turn`);
+    game = moveToNextPlayer(game);
+    return game;
+};
+
 const run = async () => {
     console.log("Hello. Let's play a game of dice.");
     let game = await setupGame();
     while (game.players.length > 1) {
-
+        game = await takeTurn(game);
     }
     let winner = game.players.pop();
     console.log(`${winner.name} has won!`)
